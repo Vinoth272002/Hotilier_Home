15d5fb528af1c4f07430e59046aae7fbfbd8c53e
Vinoth272002 <121481723+Vinoth272002@users.noreply.github.com>
2023-03-06 10:14:20 +0530

    Add files via upload
    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+$(function(){
+    // "use strict";
+    //Testimonial
+    $(".testimonial-carousel").owlCarousel({
+        autoplay:true,
+        smartSpeed:1000,
+        margin:25,
+        dots:false,
+        loop:true,
+        nav:true,
+        navText:[
+            `<i class="bi bi-arrow-left"></i>`,
+            `<i class="bi bi-arrow-left"></i>`,
+        ],
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            }
+        }
+    });
+})(jQuery);
